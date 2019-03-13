75c44c50808a61d28712fcc4ae64e342b4e3e1d4
fun <2419584260@qq.com>
2019-03-13 22:03:12 +0800

    todolist demo

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import './index.css';
-import App from './App';
+import TodoApp from './views/TodoApp';
+import todoApp from './store/reducers'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(todoApp)
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp/>
+  </Provider>,
+  document.getElementById('root')  
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
